4c1a3b6ce44c3f3cc44e78fba1f9da20a2650adc
tanvir <sarricktanvir>
2021-06-07 10:35:53 +0600

    html completed

---
+++ lab2/page-1.php
@@ -0,0 +1,58 @@
+<html>
+<head>
+    <title>Page One</title>
+</head>
+<body>
+    <form action="">
+        <table border="2" align="center">
+            <tr>
+                <td colspan="2" align="center"><h3>User Form</h3></td>
+            </tr>
+            <tr>
+                <td>First Name</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Enter Password</td>
+                <td><input type="password"></td>
+            </tr>
+            <tr>
+                <td>Enter Address</td>
+                <td><textarea name="textarea" cols="" rows=""></textarea></td>
+            </tr>
+            <tr rowspan="4">
+                <td>Select Game</td>
+                <td><input type="checkbox">Hockey <br>
+                <input type="checkbox">Football <br> 
+                <input type="checkbox">Batminton <br>
+                <input type="checkbox">Cricket  <br>
+                <input type="checkbox">Volleyball
+                </td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td><input type="radio">Male <input type="radio">Female</td>
+            </tr>
+            <tr>
+                <td>Select ur age</td>
+                <td>
+                    <select>
+                        <option value="">select</option>
+                        <option value="">15</option>
+                        <option value="">17</option>
+                        <option value="">20</option>
+                        <option value="">25</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                <input type="submit" value="Click me">
+                <input type="submit" value="Reset">
+                <input type="submit" value="Submit Form">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ lab2/pqge-2.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <title>Page Two</title>
+</head>
+<body>
+    <h2 align="center">American International University-Bangladesh</h2>
+    <hr>
+    <i>Web Programming (sec:D)</i> <br>
+    <b align="center">mid term exam</b>
+    <hr>
+    <h5 align="center">Choose</h5>
+    <form action="">
+        <table border="2" align="center">
+            <tr>
+            <td><a href="https://www.aiub.edu/">aiub</a></td>
+            <td>American International University-Bangladesh <br> <hr></td>
+            <td><ul>
+                    <li>C</li>
+                    <li>S</li>
+                </ul>
+            </td>
+            </tr>
+            <tr>
+                <td>DEPT</td>
+                <td><input type="radio">CS 
+                <input type="radio">CSE
+                <input type="radio">CIS
+                <input type="radio">CSSE</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td align="center"><input type="submit" value="Submit"></td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
