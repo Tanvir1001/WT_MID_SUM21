a00c95a028cc731bbe274fd516f4e81703fdf316
tanvir <sarricktanvir>
2021-06-14 10:57:35 +0600

    lab3 completed

--- lab3/problem2.php
+++ lab3/problem2.php
@@ -5,20 +5,20 @@
 </head>
 <body>
     <?php
-        $marks=45;
+        $marks=35;
         if($marks>=90)
             {
                 echo "The mark is: A+";
             }
-        elseif($marks>=80 and $marks<90)
+        elseif($marks>=80 && $marks<90)
             {
                 echo "The mark is: A";
             }
-        elseif($marks>=70 and $marks<80)
+        elseif($marks>=70 && $marks<80)
             {
                 echo "The mark is: B";
             }
-        elseif($marks>=60 and $marks<70)
+        elseif($marks>=60 && $marks<70)
             {
                 echo "The mark is: C";
             }
